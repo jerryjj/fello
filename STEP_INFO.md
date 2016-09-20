bc1d86380205dcfb89a3bc7cf48fc5041982b74c
Jerry Jalava <jerry.jalava@qvik.fi>
2016-09-20 19:18:47 +0300

    Step three

--- STEP_INFO.md
+++ STEP_INFO.md
@@ -1,21 +1,16 @@
-# Good job!
+# Wow, you are on fire! ;)
 
-Welcome to the part two, of this demo.
+Welcome to the part three, of this demo.
 The next instructions make the assumption that you have already finished the
-previous step.
+previous steps.
 
-* 1. Enable Google Authentication in your Firebase project
-
-Go to your [Firebase console](https://console.firebase.google.com) and navigate to
-Auth / SIGN_IN METHOD. From there enable the Google Sign-in provider.
-
-* 2. Deploy new Database rules
+* 1. Deploy new Database rules
 
 ```sh
 firebase deploy --only database
 ```
 
-* 3. Test the application locally
+* 2. Test the application locally
 
 ```sh
 npm run dev
@@ -23,7 +18,7 @@ npm run dev
 
 Login with your Google account (and log out)
 
-* 4. Deploy this new version to to Firebase Hosting
+* 3. Deploy this new version to to Firebase Hosting
 
 ```sh
 npm run deploy
--- src/client/js/main.js
+++ src/client/js/main.js
@@ -11,6 +11,9 @@ class Fello {
 
     this.auth = firebase.auth();
     this.db = firebase.database();
+
+    // Reference to the /messages/ database path.
+    this.messagesRef = this.db.ref('messages');
   }
 
   run() {
@@ -68,6 +71,44 @@ class Fello {
     this.user.id = user.uid;
   }
 
+  /* Data handling methods */
+
+  loadMessages() {
+    // Make sure we remove all previous listeners.
+    this.messagesRef.off();
+
+    // Loads the last 15 messages and listen for new ones.
+    const setMessage = (data) => {
+      if (!data) {
+        return;
+      }
+      this.displayMessage(data.key, data.val());
+    };
+    this.messagesRef.limitToLast(15).on('child_added', setMessage);
+    this.messagesRef.limitToLast(15).on('child_changed', setMessage);
+  }
+
+  writeNewMessage(uid, user, messageData) {
+    let msg = {
+      uid: uid,
+      username: user.username,
+      profileImage: user.profileImage,
+      body: messageData.body
+    };
+
+    // Get a key for a new Post.
+    const newMsgKey = this.messagesRef.push().key;
+
+    // Lets store the message to two locations simultaneously
+    let updates = {};
+    updates[`/messages/${newMsgKey}`] = msg; // Everybody reads this
+    updates[`/users-messages/${uid}/${newMsgKey}`] = msg; // Only read for the logged in user
+
+    this.db.ref().update(updates).then((data) => {
+      this.updateMeBlock();
+    });
+  }
+
   /* View handling methods, nothing spectacular here... */
 
   showPage(page) {
@@ -92,11 +133,17 @@ class Fello {
         this.activePage = 'home';
 
         this.updateMeBlock();
+        this.clearMessages();
+        this.loadMessages();
 
         document.getElementById('me-column').style.display = 'block';
+        document.getElementById('post-row').style.display = 'block';
 
         if (!firebase.auth().currentUser) {
           document.getElementById('me-column').style.display = 'none';
+          document.getElementById('post-row').style.display = 'none';
+        } else {
+          this.enablePostForm();
         }
 
         document.getElementById('page-home').style.display = 'block';
@@ -147,6 +194,121 @@ class Fello {
 
     usernameEl.innerText = this.user.username;
     profileImgEl.src = profileUrl;
+
+    // Lets update message count of our own messages
+    this.db.ref(`/users-messages/${this.user.id}`).on('value', (data) => {
+      if (!data) {
+        return;
+      }
+      if (document.getElementsByClassName('my-message-count').length) {
+        document.getElementsByClassName('my-message-count')[0].innerText = data.numChildren();
+      }
+    });
+  }
+
+  clearMessages() {
+    document.getElementById('feed-row').getElementsByClassName('items')[0].innerHTML = '';
+  }
+
+  /* Renders single message item to DOM */
+  displayMessage(key, data) {
+    const containerElement = document.getElementById('feed-row').getElementsByClassName('items')[0];
+    let el = this.createMessageElement(key, data);
+    if (!document.getElementById(`msg-${key}`)) {
+      containerElement.insertBefore(el, containerElement.firstChild);
+    }
+  }
+
+  createMessageElement(key, data) {
+    const userId = firebase.auth().currentUser ? firebase.auth().currentUser.uid : null;
+
+    let html = '';
+    const profileUrl = data.profileImage ? data.profileImage : 'http://placekitten.com/62/62';
+
+    let bodyHtml = '<p class="body"></p>';
+
+    if (userId === data.uid) {
+      html = `<div id="msg-${key}" class="media media-me well">
+        <div class="media-body media-left">
+          <blockquote class="blockquote-reverse">
+            ${bodyHtml}
+            <footer class="msg-footer">${data.username}</footer>
+          </blockquote>
+        </div>
+        <div class="media-right">
+          <a>
+            <img class="media-object" src="${profileUrl}" width="64" height="64" alt="">
+          </a>
+        </div>
+      </div>`;
+    } else {
+      html = `<div id="msg-${key}" class="media media-other well">
+        <div class="media-left">
+          <a>
+            <img class="media-object" src="${profileUrl}" width="64" height="64" alt="">
+          </a>
+        </div>
+        <div class="media-body">
+          <blockquote>
+            ${bodyHtml}
+            <footer class="msg-footer">${data.username}</footer>
+          </blockquote>
+        </div>
+      </div>`;
+    }
+
+    // Create the DOM element from the HTML.
+    const div = document.createElement('div');
+    div.innerHTML = html;
+    let postElement = div.firstChild;
+    if (document.getElementById(`msg-${key}`)) {
+      postElement = document.getElementById(`msg-${key}`);
+    }
+
+    let bodyTxt = data.body;
+    if (bodyTxt.length > 140) {
+      bodyTxt = bodyTxt.substr(0, 140) + '...';
+    }
+
+    // Set values.
+    if (postElement.getElementsByClassName('body').length) {
+      postElement.getElementsByClassName('body')[0].innerText = bodyTxt;
+    }
+
+    return postElement;
+  }
+
+  enablePostForm() {
+    const postForm = document.getElementById('post-form');
+    const bodyInput = document.getElementById('body-i');
+
+    postForm.onsubmit = (e) => {
+      e.preventDefault();
+
+      const userId = firebase.auth().currentUser.uid;
+      if (!userId) { // Shouldn't never happen
+        alert('not authenticated!');
+        return;
+      }
+
+      if (bodyInput.value) {
+        let bodyText = bodyInput.value;
+
+        // Fetch the user info as a precaution from the DB first
+        this.db.ref(`/users/${userId}`).once('value').then((snapshot) => {
+          let user = snapshot.val();
+          this.writeNewMessage(
+            firebase.auth().currentUser.uid,
+            user,
+            {
+              body: bodyText
+            }
+          );
+        });
+      }
+
+      e.target.reset();
+    };
   }
 
   /* Static methods */
